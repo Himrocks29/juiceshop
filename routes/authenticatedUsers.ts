4dbdcd80da67cc0e874fa3ffac31a114fc1bc6b3
Himanshu Sharma <himrocks@Himanshus-MacBook-Air.local>
2025-04-04 15:46:50 +0530

    JWT-Decode-Fix

--- routes/authenticatedUsers.ts
+++ routes/authenticatedUsers.ts
@@ -5,20 +5,29 @@
 import { type Request, type Response, type NextFunction } from 'express'
 import { UserModel } from '../models/user'
 import { decode } from 'jsonwebtoken'
+import {verify, JWTPayload} from 'jsonwebtoken' //import jwt verify 
 import * as security from '../lib/insecurity'
 
 async function retrieveUserList (req: Request, res: Response, next: NextFunction) {
   try {
     const users = await UserModel.findAll()
-
+    const secret = process.env.JWT_SECRET || ''
     res.json({
       status: 'success',
       data: users.map((user) => {
         const userToken = security.authenticatedUsers.tokenOf(user)
         let lastLoginTime: number | null = null
         if (userToken) {
-          const parsedToken = decode(userToken, { json: true })
-          lastLoginTime = parsedToken ? Math.floor(new Date(parsedToken?.iat ?? 0 * 1000).getTime()) : null
+          try{
+            const parsedToken = verify(userToken, JWT_SECRET) as JwtPayload
+            if(parsedToken && parsedToken.iat){
+              lastLoginTime = Math.floor(new Date(parsedToken.iat * 1000).getTime())
+            }
+          }catch (error){
+            console.error('Error verifying token:', error)
+          }
+          //const parsedToken = decode(userToken, { json: true })
+          //lastLoginTime = parsedToken ? Math.floor(new Date(parsedToken?.iat ?? 0 * 1000).getTime()) : null
         }
 
         return {
