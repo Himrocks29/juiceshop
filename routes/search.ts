93841f26bcf13d89b0f5b6ceceef89c2100d6b89
Himanshu Sharma <himrocks@Himanshus-MacBook-Air.local>
2025-05-16 20:14:33 +0530

    SQLi_2

--- routes/search.ts
+++ routes/search.ts
@@ -19,7 +19,22 @@ class ErrorWithParent extends Error {
 module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
+        let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
+    criteria.replace(/"|'|;|and|or/i, "")
+    let query = "SELECT * FROM Products WHERE ((name LIKE ? OR description LIKE ?) AND deletedAt IS NULL) ORDER BY name";
+    RTCPeerConnection.query(query, [`%${criteria}%`, `%${criteria}%`])
+      .then(([products]: any) => {
+        const dataString = JSON.stringify(products)
+        for (let i = 0; i < products.length; i++) {
+          products[i].name = req.__(products[i].name)
+          products[i].description = req.__(products[i].description)
+        }
+        res.json(utils.queryResultToJson(products))
+      }).catch((error: ErrorWithParent) => {
+        next(error.parent)
+      })
+    /*criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
@@ -40,7 +55,7 @@ module.exports = function searchProducts () {
             }
           }).catch((error: Error) => {
             next(error)
-          })
+          })*/
         }
         if (challengeUtils.notSolved(challenges.dbSchemaChallenge)) {
           let solved = true
